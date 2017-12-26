bea8ad8411c502e29fe3228ec0de257ed3d72307
yl616775291@163.com <17111451@cnsuning.com>
2017-12-26 15:14:47 +0800

    constant all define in a common file && support iphone

---
+++ STTabbarConstant.h
@@ -0,0 +1,12 @@
+//
+//  STTabbarConstant.h
+//  Pods
+//
+//  Created by yq on 2017/12/26.
+//
+
+#ifndef STTabbarConstant_h
+#define STTabbarConstant_h
+
+
+#endif /* STTabbarConstant_h */
---
+++ STTabbarConstant.m
@@ -0,0 +1,8 @@
+//
+//  STTabbarConstant.m
+//  STTabbarController
+//
+//  Created by yq on 2017/12/26.
+//
+
+#import <Foundation/Foundation.h>
